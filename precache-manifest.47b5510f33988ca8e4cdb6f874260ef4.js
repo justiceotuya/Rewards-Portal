b1a68219a3687345e9cc68e3bf5132cdace1da22
Justice Otuya <justice.otuya@digitalpulse.ng>
2019-02-13 19:38:18 +0100

    Updates

---
+++ precache-manifest.47b5510f33988ca8e4cdb6f874260ef4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "01bacbe8332167bc8685",
+    "url": "/rewards-portal/static/css/main.e6797605.chunk.css"
+  },
+  {
+    "revision": "01bacbe8332167bc8685",
+    "url": "/rewards-portal/static/js/main.01bacbe8.chunk.js"
+  },
+  {
+    "revision": "2bd2c6bbb233c6c230e8",
+    "url": "/rewards-portal/static/js/runtime~main.2bd2c6bb.js"
+  },
+  {
+    "revision": "bbd462de282325e84a4d",
+    "url": "/rewards-portal/static/css/2.489ae067.chunk.css"
+  },
+  {
+    "revision": "bbd462de282325e84a4d",
+    "url": "/rewards-portal/static/js/2.bbd462de.chunk.js"
+  },
+  {
+    "revision": "0576f0102b99e4fe5cbb768cae820c29",
+    "url": "/rewards-portal/static/media/user-3.0576f010.jpg"
+  },
+  {
+    "revision": "6b3d4fd5f550175f47242f23aa4b581a",
+    "url": "/rewards-portal/static/media/icons.6b3d4fd5.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/rewards-portal/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/rewards-portal/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/rewards-portal/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/rewards-portal/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "547fa52b9a07da4ff844fae5622ff9b3",
+    "url": "/rewards-portal/index.html"
+  }
+];
